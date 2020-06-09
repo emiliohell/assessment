9975aae7bc9c66c83a0f165931346dc1c012af4d
Emilio <emilio.hellberg@hyperisland.se>
2020-06-09 20:37:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+window.onload = function () {
+  const menuItems = [
+    "intro",
+    "overview",
+    "emilio",
+    "k6",
+    "k7",
+    "s3",
+    "s4",
+    "s5",
+    "s6",
+    "c3",
+    "c4",
+    "future",
+  ];
+
+  document.getElementById(menuItems[0]).onclick = () => {
+    docSlider.jumpPage(0);
+    return false;
+  };
+
+  document.getElementById(menuItems[1]).onclick = () => {
+    docSlider.jumpPage(1);
+    return false;
+  };
+
+  document.getElementById(menuItems[2]).onclick = () => {
+    docSlider.jumpPage(2);
+    return false;
+  };
+
+  document.getElementById(menuItems[3]).onclick = () => {
+    docSlider.jumpPage(3);
+    return false;
+  };
+
+  document.getElementById(menuItems[4]).onclick = () => {
+    docSlider.jumpPage(4);
+    return false;
+  };
+
+  document.getElementById(menuItems[5]).onclick = () => {
+    docSlider.jumpPage(5);
+    return false;
+  };
+
+  document.getElementById(menuItems[6]).onclick = () => {
+    docSlider.jumpPage(6);
+    return false;
+  };
+
+  document.getElementById(menuItems[7]).onclick = () => {
+    docSlider.jumpPage(7);
+    return false;
+  };
+
+  document.getElementById(menuItems[8]).onclick = () => {
+    docSlider.jumpPage(8);
+    return false;
+  };
+
+  document.getElementById(menuItems[9]).onclick = () => {
+    docSlider.jumpPage(9);
+    return false;
+  };
+
+  document.getElementById(menuItems[10]).onclick = () => {
+    docSlider.jumpPage(10);
+    return false;
+  };
+
+  document.getElementById(menuItems[11]).onclick = () => {
+    docSlider.jumpPage(11);
+    return false;
+  };
+};
